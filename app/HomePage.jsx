0008a5c80d77281772af8018b7f1f76b678c6226
bluesky2006 <simon.busby@gmail.com>
2025-07-04 17:05:15 +0100

    Update file structure and create empty component files

---
+++ app/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+
+function HomePage() {
+
+
+  
+
+  return (
+    <View>
+      <Text>Here is some text!!!</Text>
+    </View>
+  );
+}
+
+export default HomePage;
