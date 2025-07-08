1525128a353f52bc5f1d65685099489b0126f8da
bluesky2006 <simon.busby@gmail.com>
2025-07-08 15:16:09 +0100

    Add response_preview route

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" />
-    <Stack.Screen name="camera" />
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="camera" />
+      <Stack.Screen name="response_preview" />
+    </Stack>
+  );
 }
