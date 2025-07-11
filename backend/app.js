5fee47dcba51b0c52592811c23de9f925ed7ca1b
laureneyfs <laureneyfs@gmail.com>
2025-07-11 11:24:59 +0100

    increase json post filesize limit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { handleCustomErrors, handleServerErrors } = require("./errors");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use("/api", apiRouter);
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
