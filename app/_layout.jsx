3df9cef5f4c1cda06a411fc5f013c3728bf3be2d
bluesky2006 <simon.busby@gmail.com>
2025-07-11 14:54:12 +0100

    Initalise authentication support

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="home" />
       <Stack.Screen name="recipe_detail" />
       <Stack.Screen name="camera_screen" />
       <Stack.Screen name="image_picker" />
