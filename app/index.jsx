27726987cc13a8e02ec0e5c791858d4d8f725896
Darkon101 <samin.taseen@outlook.com>
2025-07-14 16:38:27 +0100

    add sign in page for merge

---
+++ app/index.jsx
@@ -0,0 +1,82 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  isErrorWithCode,
+  isSuccessResponse,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import { useContext, useState } from "react";
+import { Button, View } from "react-native";
+import { fetchUserByEmail, postUser } from "../api/api";
+import {UserContext} from '../contexts/UserContext'
+import { router } from "expo-router";
+
+export default function Main() {
+  const { loggedInUserId, setLoggedInUserId } = useContext(UserContext);
+  const [user, setUser] = useState(null);
+
+  GoogleSignin.configure();
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        setUser(response.data);
+        console.log(user.user);
+        fetchUserByEmail(user.user.email).then((res)=>{
+          setLoggedInUserId(res._id)
+        }).catch((err)=> {
+          postUser(user.user.email, user.user.name).then((res)=>{
+            setLoggedInUserId(res._id)
+          }).catch((err)=>{
+            console.log(err)
+          })
+        }).finally(()=>{
+          router.navigate("/home");
+        })
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      setUser(null); // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <View>
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={() => {
+            signIn();
+          }}
+        />
+        <Button onPress={() => signOut()} title="Sign Out" disabled={!user} />
+      </View>
+    </>
+  );
+}
